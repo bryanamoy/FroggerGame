c6eb1b50e63c3f5a8b7f0e7e80dc23e8e85be8a8
Bryan Moy <bryanmoy@gmail.com>
2019-10-23 16:37:53 -0400

    Update lab7wrapper.c

--- lab7wrapper.c
+++ lab7wrapper.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 
-extern int lab7(void);
+extern int frogger(void);
 extern void UART0Handler(void);
 extern void Timer0Handler(void);
 extern void PortAHandler(void);
 
 int main(void)
 {
-    lab7();
+    frogger();
 }
